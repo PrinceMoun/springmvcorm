8a55ad1b89e3b36f1bc21312773244e6bc9a0c70
Prince Moun <princemoun30@gmail.com>
2020-06-23 00:33:58 +0530

    Initial conmit

---
+++ springmvcorm/src/main/java/com/prince/spring/springmvcorm/user/dao/UserDao.java
@@ -0,0 +1,15 @@
+package com.prince.spring.springmvcorm.user.dao;
+
+import java.util.List;
+
+import com.prince.spring.springmvcorm.user.entity.User;
+
+public interface UserDao {
+	
+	int create(User user);
+	
+	List<User> findUsers();
+	
+	User findUser(Integer id);
+
+}
---
+++ springmvcorm/src/main/java/com/prince/spring/springmvcorm/user/services/UserService.java
@@ -0,0 +1,15 @@
+package com.prince.spring.springmvcorm.user.services;
+
+import java.util.List;
+
+import com.prince.spring.springmvcorm.user.entity.User;
+
+public interface UserService {
+	
+	int save(User user);
+	
+	List<User> getUsers();
+	
+	User getUser(Integer id);
+
+}
